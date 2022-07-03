b4eb21ef3eb5b0c901f68e99d5f8f4721f169d2c
xiaoxian521 <1923740402@qq.com>
2022-07-03 16:22:02 +0800

    faet: add `nextjs` example

---
+++ packages/nextjs/pages/index.js
@@ -0,0 +1,15 @@
+import { Icon as IconifyIcon } from "@iconify-icon/react"
+
+export default function Home() {
+  return (
+    <div className="content">
+      <p>NextJs Version</p>
+      <IconifyIcon className="icon" icon="openmoji:1st-place-medal" />
+      <IconifyIcon className="icon" icon="vscode-icons:file-type-access2" />
+      <IconifyIcon className="icon" icon="logos:bigpanda" />
+      <IconifyIcon className="icon" icon="fxemoji:2hearts" />
+      <IconifyIcon className="icon" icon="flat-color-icons:businessman" />
+      <IconifyIcon className="icon" icon="cif:cn" />
+    </div>
+  )
+}
