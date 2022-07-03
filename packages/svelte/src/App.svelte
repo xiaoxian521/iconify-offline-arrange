6626c4e3cdc0ac9d4ee79eef5385e03da4c231c6
xiaoxian521 <1923740402@qq.com>
2022-07-03 16:35:52 +0800

    feat: add `svelte` example

---
+++ packages/svelte/src/App.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import Icon from '@iconify/svelte';
+</script>
+
+<main>
+ <div class="content">
+    <p>Svelte Version</p>
+    <Icon class="icon" icon="openmoji:1st-place-medal" />
+    <Icon class="icon" icon="vscode-icons:file-type-access2" />
+    <Icon class="icon" icon="logos:bigpanda" />
+    <Icon class="icon" icon="fxemoji:2hearts" />
+    <Icon class="icon" icon="flat-color-icons:businessman" />
+    <Icon class="icon" icon="cif:cn" />
+  </div>
+</main>
